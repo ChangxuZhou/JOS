d4d53c0e8b36da4ef49ec65d50e64fa0176417cf
tonnylyz <me@lyzde.com>
2017-03-28 13:48:39 +0800

    Clion ready

---
+++ cmake-build-debug/CMakeFiles/os_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os_test.dir/lib/print.c.o"
+  "CMakeFiles/os_test.dir/lib/printf.c.o"
+  "CMakeFiles/os_test.dir/test.c.o"
+  "os_test.pdb"
+  "os_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/os_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
